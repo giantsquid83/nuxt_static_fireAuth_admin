8ff0c3d16ab04e9db3854cac827f644a07112fb0
giantsquid83 <accou@rv7.ru>
2020-08-15 10:22:46 +0300

    no useless folder

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['adminRoute'] = require('..\\middleware\\adminRoute.js')
-middleware['adminRoute'] = middleware['adminRoute'].default || middleware['adminRoute']
-
-export default middleware
